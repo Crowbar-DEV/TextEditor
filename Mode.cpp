361f428210afa9a572dcb9d35c3e432734877fca
Tyler Spaeth <tspaeth2@wisc.edu>
2023-06-09 03:11:41 +0000

    state interface

---
+++ Mode.cpp
@@ -0,0 +1,3 @@
+using namespace std;
+
+enum Mode {INSERT,VIEW};
---
+++ State.h
@@ -0,0 +1,18 @@
+#include <fsteam>
+
+class State {
+	private:
+		Mode::Mode currentMode;
+	
+	protected:
+		std::string timeStamp;
+		bool upToDate;
+		std::string file;
+		fstream fileStream;
+	
+	public:
+		State();
+		virtual void setMode(Mode::Mode newMode);
+		virtual Mode::Mode getMode();
+
+}
