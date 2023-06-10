a061fe59324aab8d7ee784ec53e3cd0f7a58ba05
Gavin <gmcellistrem@gmail.com>
2023-06-09 19:45:16 -0500

    shit

---
+++ FileHandler.cpp
@@ -0,0 +1,9 @@
+#include "FileHandler.h"
+
+void FileHandler::storeFileInfo(std::string name){
+	currentFile = std::fstream("test.txt");
+	buffer = currentFile.rdbuf();
+}
+
+
+
--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,46 @@
 #include "FileHandler.h"
 #include "State.h"
+#include <format>
 
 int main(){
+	std::fstream file ("test.txt", std::ios::ate);
+	int end = file.tellp();	
+	std::cout << end<< "\n";
+	char arr[end];
+
+	file.seekg(0);
+
+	std::fstream buf;
+
+	file>>arr;
+
+	buf<<arr;
+	
+	file.close();
+	char c;
+	for(int i = 0; i< 50; i++){
+		buf.get(c);
+		std::cout<< c;
+	}
+
+	//std::cout<< file.tellg()<< "\n" ;
+	//file.close();
+	//std::fstream file ("test.txt");
+	//std::filebuf * fileBuf = file.rdbuf();
+	//std::filebuf * restoreBuf;
+	//std::system("clear");
+	//std::cout <<fileBuf;	
+	//std::vector<char> mem;
+	//char c = fileBuf->sbumpc();
+
+	//while(c != EOF){
+	//	mem.push_back(c);
+	//	c = fileBuf->sbumpc();
+	//}
+	//file.close();
+	//
+	//for(char c : mem){
+	//	std::cout<<c<<"\n";
+	//}
 	return 0;
 }
