b940a83dda0356e181703b9d7d46fc5789217be8
Tyler Spaeth <tspaeth2@wisc.edu>
2023-06-24 19:29:31 +0000

    cursor

--- outline/Cursor.hpp
+++ outline/Cursor.hpp
@@ -1,2 +1,9 @@
 class Cursor {
+	public:
+		const std::pair<int,int> getCursorPos();
+		void setCursorPos(int line, int col);
+		const int getLine();
+		const int getCol();
+	private:
+		std::pair<int,int> cursorPos;
 };
